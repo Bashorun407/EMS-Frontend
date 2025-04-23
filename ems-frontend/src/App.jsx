63060fbabb17d0059a9bf5f7ae0ba53d62e14013
Bashorun Olusheyi <boboladvat@gmail.com>
2025-04-23 20:00:44 +0100

    used BrowseRouter, Routes & Route in App.jsx file to list employees

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,13 +2,21 @@
 import './App.css'
 import ListEmployeeComponents from './components/ListEmployeeComponents'
 import HelloWorld from './HelloWorld'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      <ListEmployeeComponents />
+      <BrowserRouter>
+        <Routes>
+          {/* //http://localhost:3500 */}
+          <Route path='/' element={<ListEmployeeComponents />}></Route>
+          {/* // http://localhost:3500/api/employee/allEmployees */}
+          <Route path='/allEmployees' element={<ListEmployeeComponents />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
